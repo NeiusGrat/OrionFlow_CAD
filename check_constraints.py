3939ce33e076c7d0af5a3494fc8164cf5fe7af40
sahilmaniyar888 <156301258+sahilmaniyar888@users.noreply.github.com>
2026-01-16 00:00:33 +0530

    chore: Remove various debug and utility scripts.

--- check_constraints.py
+++
@@ -1,5 +0,0 @@
-from build123d import *
-import build123d
-print([x for x in dir(build123d) if "Constraint" in x])
-print([x for x in dir(build123d) if "Fixed" in x])
-print([x for x in dir(build123d) if "Rigid" in x])
--- check_sig.py
+++
@@ -1,3 +0,0 @@
-from build123d import export_stl
-import inspect
-print(inspect.signature(export_stl))
