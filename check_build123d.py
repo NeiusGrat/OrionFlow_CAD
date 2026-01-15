e1f3bf39eaa544f32300f8b85bb84bbb563fb032
sahilmaniyar888 <156301258+sahilmaniyar888@users.noreply.github.com>
2026-01-16 00:00:33 +0530

    chore: Remove various debug and utility scripts.

--- check_build123d.py
+++
@@ -1,5 +0,0 @@
-from build123d import *
-import build123d
-print("Radius" in dir(build123d))
-print("Rad" in dir(build123d))
-print([x for x in dir(build123d) if "Rad" in x])
--- dump_dir.py
+++
@@ -1,4 +0,0 @@
-import build123d
-with open("build123d_dir.txt", "w") as f:
-    for x in dir(build123d):
-        f.write(x + "\n")
